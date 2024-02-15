6e8925d96fbad7b4258171bcc67dc2781db108df
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-02-15 13:32:19 +0100

    structure

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,4 +1,5 @@
 import hamburgerMenu from '../assets/hamburger-menu.png'
+import meditateLogo from '../../public/meditate-logo.svg'
 import './Header.css'
 
 function Header(){
@@ -9,6 +10,7 @@ function Header(){
         <button className="eng">EN</button>
       </div>
       <div className="logo">
+        <img src={meditateLogo} className='meditate-logo'></img>
         <p className="logo-text">Meditate Like A Buddah!</p>
       </div>
       <button className="hamburger-menu">
--- src/MainContent/MainContent.jsx
+++ src/MainContent/MainContent.jsx
@@ -1,8 +1,25 @@
+import meditateLogo from '../../public/meditate-logo.svg'
+import './MainContent.css'
+
 function MainContent(){
   return(
-    <>
-
-    </>
+    <div className="main-content">
+      <h1 className="title">Welcome to Meditate Like A Buddah</h1>
+      <img src={meditateLogo} className='meditate-logo-main'></img>
+      <div className="how-to-meditate">
+        <h2 className='are-you-ready'>Are you ready to meditate?</h2>
+        <ol className="list">
+          <li>Take a seat</li>
+          <li>Set a time limit</li>
+          <li>Notice your body</li>
+          <li>Feel your breath</li>
+          <li>Notice when your mind has wandered</li>
+          <li>Be kind to your wandering mind</li>
+          <li>Close with kindness</li>
+        </ol>
+      </div>
+      <button className="get-started">Get Started</button>
+    </div>
   )
 }
 
