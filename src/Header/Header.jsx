582f09906d006ce3060a45055b3565062387c19b
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-02-19 07:46:17 +0100

    close menu onCLick outside

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
+import { useState,useEffect, useRef } from 'react'
 import hamburgerMenu from '../assets/hamburger-menu.png'
 import meditateLogo from '../../public/meditate-logo.svg'
 import './Header.css'
 
 function Header(){
-  const [openMenu, setOpenMenu] = useState(true);
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuRef = useRef(null);
 
   const handleOpenMenu = () => {
     setOpenMenu(!openMenu);
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setOpenMenu(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return(
-    <div className="header">
-      <div className="language">
-        <button className="ita">IT</button>
-        <button className="eng">EN</button>
-      </div>
-      <div className="logo">
-        <img src={meditateLogo} className='meditate-logo'></img>
-        <p className="logo-text">Meditate Like A Buddah!</p>
-      </div>
-      <button className='menu-btn' onClick={handleOpenMenu}>
-        <img src={hamburgerMenu} className="hamburger-menu-img"></img>
-      </button>
-      <ul className={`${openMenu ? "hamburger-menu" : "hamburger-menu-open"}`}>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </div>
+    <header ref={menuRef}>
+      <nav className="header">
+        <div className="language">
+          <button className="ita">IT</button>
+          <button className="eng">EN</button>
+        </div>
+        <div className="logo">
+          <img src={meditateLogo} className='meditate-logo'></img>
+          <p className="logo-text">Meditate Like A Buddah!</p>
+        </div>
+        <button className='menu-btn' onClick={handleOpenMenu}>
+          <img src={hamburgerMenu} className="hamburger-menu-img"></img>
+        </button>
+        <ul className={`${openMenu ? "hamburger-menu-open" : "hamburger-menu"}`}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
