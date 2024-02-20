d6bd8b5c4485cc6cb51e77898f46905d964fde9b
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-02-20 12:10:08 +0100

    media query

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -6,16 +6,18 @@ import './Footer.css'
 function Footer(){
   return(
     <div className='footer'>
-      <small className='footer-text'>© 2024 Veselin Hristov. All rights reserved.</small>
-      <a className='instagram' href='https://instagram.com/vese.10?igshid=OGQ5ZDc2ODk2ZA==' target='_blank' rel='nooper'>
+      <div className='social-logos'>
+        <a className='instagram' href='https://instagram.com/vese.10?igshid=OGQ5ZDc2ODk2ZA==' target='_blank' rel='nooper'>
         <img className="instagram-img" src={instagram} alt="Instagram logo" /> 
-      </a>
-      <a className='github' href='https://github.com/Vese10' target='_blank' rel='nooper'>
-        <img className="github-img" src={github} alt="GitHub logo" /> 
-      </a>
-      <a className='linkedin' href='https://www.linkedin.com/in/veselin-hristov-53b750216/' target='_blank' rel='nooper'>
-        <img className="linkedin-img" src={linkedin} alt="LinkedIn logo" /> 
-      </a>
+        </a>
+        <a className='github' href='https://github.com/Vese10' target='_blank' rel='nooper'>
+          <img className="github-img" src={github} alt="GitHub logo" /> 
+        </a>
+        <a className='linkedin' href='https://www.linkedin.com/in/veselin-hristov-53b750216/' target='_blank' rel='nooper'>
+          <img className="linkedin-img" src={linkedin} alt="LinkedIn logo" /> 
+        </a>
+      </div>
+      <small className='footer-text'>© 2024 Veselin Hristov. All rights reserved.</small>
     </div>
   )
 }
