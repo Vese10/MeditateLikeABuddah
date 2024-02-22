820f65e9ba95e111e47cc16f834f04699207dd2b
Vese10 <146728778+Vese10@users.noreply.github.com>
2024-02-22 16:17:10 +0100

    background images added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import backgroundSmall from './assets/backgraound-img-small.jpg'
+import backgroundBig from './assets/background-big.jpg'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='images'>  
+      <img src={backgroundSmall} className='background-small' alt='Leaves'/>
+      <img src={backgroundBig} className='background-big' alt='Leaves'/>
+      <App className='app' />
+    </div>
   </React.StrictMode>,
 )
